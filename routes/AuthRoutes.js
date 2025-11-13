ae8411bfa105a2380711eebe5207fa345442d03d
Tania <taniafernandes787@gmail.com>
2025-11-13 21:28:00 +0530

    user_12

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,16 +1,10 @@
 import express from 'express';
 import { loginUserCtrl, registerUserCtrl } from '../controllers/authController.js';
-// import { registerUserCtrl , loginUserCtrl } from '../controllers/AuthController.js';
 
-// const router = express.Router();
-
-// // Include /auth in the router
-// router.post('/auth/register', registerUserCtrl);
-// router.post('/auth/login', loginUserCtrl);
 
 const router = express.Router();
 
-router.post('/auth/register', registerUserCtrl)
-router.post('/auth/login', loginUserCtrl);
+router.post('/register', registerUserCtrl)
+router.post('/login', loginUserCtrl);
 
 export default router;
