ffa6cef70cb69cee61551b7607634b8898c0f563
Tania <taniafernandes787@gmail.com>
2025-11-13 21:22:04 +0530

    user_11

--- app/app.js
+++ app/app.js
@@ -66,10 +66,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-// --- Start Server ---
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 export default app;
